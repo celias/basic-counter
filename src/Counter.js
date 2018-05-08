6578f28cbc647b991f7078d034c5fd0ea1579078
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-05-08 14:30:44 -0500

    basic counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.baseState = this.state;
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  resetState = () => {
+    this.setState(
+      this.baseState 
+    );
+  }
   render() {
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment} className="full-width">Increment</button>
+        <button onClick={this.decrement} className="full-width">Decrement</button>
+        <button onClick={this.resetState} className="full-width">Reset</button>
       </section>
     );
   }
